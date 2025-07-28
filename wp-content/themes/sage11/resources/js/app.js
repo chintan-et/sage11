672eb968438f347e4e3b4a553ace2fb41216db4f
chintan-et <chintans.encircle@gmail.com>
2025-07-28 17:40:52 +0530

    update app.js

--- wp-content/themes/sage11/resources/js/app.js
+++ wp-content/themes/sage11/resources/js/app.js
@@ -1,4 +1,14 @@
-import.meta.glob([
-  '../images/**',
-  '../fonts/**',
-]);
+import 'jquery';
+import domReady from '@roots/sage/client/dom-ready';
+import { each } from 'jquery';
+
+
+/**
+ * Application entrypoint
+ */
+domReady(async () => {
+
+  jQuery(document).ready(function ($) {
+
+  });
+});
